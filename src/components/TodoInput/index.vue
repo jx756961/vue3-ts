4f874cdefabfbee9d3efde00af51ac31691d9b09
jxlv <188586771@qq.com>
2020-12-02 21:20:01 +0800

    TodoInput add setTodoValue and KeyboardEvent

--- src/components/TodoInput/index.vue
+++ src/components/TodoInput/index.vue
@@ -9,16 +9,22 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent, ref } from "vue"
 
 export default defineComponent({
-    name: 'TodoInput'
-    /* setup(props) {
-        
-    } */
-    /* return {
-        setTodoValue
-    } */
+    name: 'TodoInput',
+     setup(props) {
+        const todoValue = ref<string>('') // 这里使用泛型string类型推导
+        const setTodoValue = (e: KeyboardEvent): void =>{ // 函数参数类型 KeyboardEvent, void 无返回值， 有返回值要定义返回值类型
+          if(e.keyCode === 13 && todoValue.value.trim().length) {
+             // 这里提交数据到 stor e 仓库
+          } // todoValue.value 去掉前后空格如果是真才提交
+        }
+        return {
+          todoValue,
+          setTodoValue
+        }
+    }
 })
 
 </script>
